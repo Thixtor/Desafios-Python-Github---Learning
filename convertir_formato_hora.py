982b527d2d5d1a9ffa3c99ba2e9e9735ef9e0763
Thixtor <sebas.sas@gmail.com>
2023-01-27 16:54:05 -0500

    Cambio para identificar PM
    Se cambia la forma para identificar si es hora PM.

--- convertir_formato_hora.py
+++ convertir_formato_hora.py
@@ -1,10 +1,10 @@
 def normal_militar(hora):
-    hora_modificada = hora.replace(":", " ").split()
+    hora_modificada = hora.split(":")
    
-    if hora_modificada[2] == "PM":
+    if "PM" in hora_modificada[1]:
         hora_modificada[0] = int(hora_modificada[0]) + 12
         print (str(hora_modificada[0])+":"+hora_modificada[1])
     else: 
         print("La hora esta en formato 12 horas.")
 
-normal_militar("10:45 AM")
+normal_militar("10:45PM")
