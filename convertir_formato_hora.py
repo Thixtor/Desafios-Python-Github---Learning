4ef9fad5d0501089a4c82d58ab1347923d2c4d78
Thixtor <sebas.sas@gmail.com>
2023-01-27 17:08:17 -0500

    Correcciones

--- convertir_formato_hora.py
+++ convertir_formato_hora.py
@@ -2,9 +2,10 @@ def normal_militar(hora):
     hora_modificada = hora.split(":")
    
     if "PM" in hora_modificada[1]:
-        hora_modificada[0] = int(hora_modificada[0]) + 12
-        print (str(hora_modificada[0])+":"+hora_modificada[1])
-    else: 
-        print("La hora esta en formato 12 horas.")
+        if int(hora_modificada[0]) != 12:
+            hora_modificada[0] = int(hora_modificada[0]) + 12
+            print (str(hora_modificada[0])+":"+hora_modificada[1][0:2])
+        else: 
+            print ("00"+":"+hora_modificada[1][0:2])
 
-normal_militar("10:45PM")
+normal_militar("12:45PM")
